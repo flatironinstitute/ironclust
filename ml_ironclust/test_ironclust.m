8f5c67fe9846889b8ee35fc0a3a7f7ad57d3455f
James Jun <jamesjun@gmail.com>
2018-04-17 18:24:13 -0400

    Added files

--- ml_ironclust/test_ironclust.m
+++ ml_ironclust/test_ironclust.m
@@ -1,15 +1,31 @@
-addpath('./mdaio');
-addpath('./JRCLUST');
+% addpath('./mdaio');
+% addpath('./JRCLUST');
+% 
+% temp_path = 'D:\JeremyMagland\K15\jrc3';
+% raw_fname = 'D:\JeremyMagland\K15\tet_K=15_1.mda';
+% prb_fname = '.\JRCLUST\prb\tetrode.prb';
+% param_fname = 'D:\JeremyMagland\K15\tet_K=15_1.meta';
+% settings_fname = 'D:\JeremyMagland\K15\settings.prm';
+% firings_out_fname = 'firings_out.mda';
+% 
+% % Clear previous 
+% try rmdir(temp_path, 's'); catch, end
+% 
+% p_ironclust(temp_path,raw_fname,prb_fname,param_fname,settings_fname,firings_out_fname);
+% 
 
-temp_path = 'D:\JeremyMagland\K15\jrc3';
-raw_fname = 'D:\JeremyMagland\K15\tet_K=15_1.mda';
-prb_fname = '.\JRCLUST\prb\tetrode.prb';
-param_fname = 'D:\JeremyMagland\K15\tet_K=15_1.meta';
-settings_fname = 'D:\JeremyMagland\K15\settings.prm';
-firings_out_fname = 'firings_out.mda';
-
-% Clear previous 
-try rmdir(temp_path, 's'); catch, end
-
-p_ironclust(temp_path,raw_fname,prb_fname,param_fname,settings_fname,firings_out_fname);
-
+%matlab -nodisplay -r 
+addpath('/home/jamesjun/Dropbox/flatiron/ironclust/ml_ironclust/'); 
+addpath('/home/jamesjun/Dropbox/flatiron/ironclust/ml_ironclust/JRCLUST'); 
+addpath('/home/jamesjun/Dropbox/flatiron/ironclust/ml_ironclust/mdaio'); 
+try 
+    p_ironclust( ...
+        '/tmp/mountainlab-tmp/tempdir_5566f41e19_pQojAF', ...
+        '/tmp/mountainlab-tmp/output_f509f39b380e8484d5a14ebd2744754cb1ee88f3_timeseries_out', ...
+        '/tmp/mountainlab-tmp/output_98828a223d010792aef9f1c7da1e6df7b2f3f279_geometry_out', ...
+        '/home/jamesjun/mountainsort_examples/examples/001_sort_datasets/datasets/synth_K10/firings.mda', ...
+        '/tmp/mountainlab-tmp/tempdir_5566f41e19_pQojAF/argfile.txt'); 
+catch
+    fprintf(2,lasterr());
+end
+%quit;
