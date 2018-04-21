6304c97e8ed501bcb9aa9728dcc43f2bf126c031
James Jun <jamesjun@gmail.com>
2018-04-20 20:32:35 -0400

    v3.2.8
    Mountainlab integration
    1. makeprm-mda added
    2. temporary output directory can be specified.
    
    For more see "change_log.txt"

--- ml_ironclust/p_ironclust.m
+++ ml_ironclust/p_ironclust.m
@@ -1,4 +1,4 @@
-function p_ironclust(temp_path, raw_fname, geom_fname, prm_fname, arg_fname)
+function p_ironclust(temp_path, raw_fname, geom_fname, prm_fname, firings_out_fname, arg_fname)
 % cmdstr2 = sprintf("p_ironclust('$(tempdir)','$timeseries$','$geom$','$firings_out$','$(argfile)');");
 
 % temp_path='/tmp/test_jrclust'
@@ -10,28 +10,34 @@ end
 % copyfile_(geom_fname, subs_dir_(raw_fname, geom_fname));
 % copyfile_(param_fname,[temp_path,'/raw.meta']);
 % copyfile_(settings_fname,[temp_path,'/settings.prm']);
-S_mda = makeStruct_(temp_path, raw_fname, geom_fname, prm_fname, arg_fname);
-jrc('makeprm-mda', raw_fname, geom_fname, prm_fname, arg_fname);
+% S_mda = makeStruct_(temp_path, raw_fname, geom_fname, prm_fname, arg_fname);
+
+% write meta, don't change jrc but create .mda2meta utility and .csv2prb
+% utility. I like having all in one place though.
+vcFile_prm = jrc3('makeprm_mda', raw_fname, geom_fname, arg_fname, temp_path, prm_fname); % vcFile_meta = strrep(raw_fname, '.mda', '.meta');
+jrc3('detectsort', vcFile_prm);
+vcFile_jrc_csv = jrc3('export-csv', vcFile_prm);
+
 % jrc('makeprm',[temp_path,'/raw.mda'],[temp_path,'/raw.prb'],[temp_path,'/settings.prm']); %creates tmp_jrclust/raw_raw.prm
-jrc3('call', 'edit_prm_file_', {struct('header_offset', 20, 'fSavePlot_RD', 0), [temp_path,'/raw_raw.prm']}, 0);
+% jrc3('call', 'edit_prm_file_', {struct('header_offset', 20, 'fSavePlot_RD', 0), [temp_path,'/raw_raw.prm']}, 0);
 
-jrc('detectsort',[temp_path,'/raw_raw.prm']);
-jrc('export-csv',[temp_path,'/raw_raw.prm']); % writes to tmp_jrclust/raw_raw.csv
-
+% jrc('detectsort',[temp_path,'/raw_raw.prm']);
+% jrc('export-csv',[temp_path,'/raw_raw.prm']); % writes to tmp_jrclust/raw_raw.csv
+% prmfile
 disp('======================================================================');
 
-firings=csvread([temp_path,'/raw_raw.csv']);
+firings = csvread(vcFile_jrc_csv);
 
 disp('======================================================================');
 disp('Shape of firings:');
 disp(size(firings));
 
-firings=firings(:,[3,1,2])';
-S_out = jrc3('call', 'file2struct_', {[temp_path, '/raw_raw.prm']}, 1);
-P = S_out.out1;
+firings = firings(:,[3,1,2])';
+P = jrc3('call', 'file2struct_', vcFile_prm);
 firings(2,:)=firings(2,:) * P.sRateHz;
 
-writemda(firings,firings_out_fname);
+writemda(firings, firings_out_fname);
+fprintf('Clustering result wrote to %s\n', firings_out_fname);
 
 end %func
 
