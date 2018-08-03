49e9a2b31053200236868d8d4dc48ebecbd91b90
James Jun <jamesjun@gmail.com>
2018-08-03 13:59:34 -0400

    v4.0.3

--- matlab/irc.m
+++ matlab/irc.m
@@ -1678,17 +1678,22 @@ viSite = int32(viSite);
 viSpk = int32(viSpk);   
 
 [cviSpkA, cvrSpkA, cviSiteA] = deal(cell(nSites,1));
-
-try
-    parfor iSite = 1:nSites %parfor speedup: 2x %parfor
-        try
-            [cviSpkA{iSite}, cvrSpkA{iSite}, cviSiteA{iSite}] = ...
-                spikeMerge_single_(viSpk, vrSpk, viSite, iSite, P);            
-        catch
-            fprint(2, 'Parfor error, retrying using a single thread.\n');
+fParfor = get_set_(P, 'fParfor', 1);
+if fParfor
+    try
+        parfor iSite = 1:nSites %parfor speedup: 2x %parfor
+            try
+                [cviSpkA{iSite}, cvrSpkA{iSite}, cviSiteA{iSite}] = ...
+                    spikeMerge_single_(viSpk, vrSpk, viSite, iSite, P);            
+            catch
+                fprint(2, 'Parfor error, retrying using a single thread.\n');
+            end
         end
+    catch
+        fParfor = 0; 
     end
-catch
+end %if
+if ~fParfor
     for iSite = 1:nSites
         try
             [cviSpkA{iSite}, cvrSpkA{iSite}, cviSiteA{iSite}] = ...
@@ -13974,101 +13979,6 @@ end %func
 
 %--------------------------------------------------------------------------
 % 6/29/17 JJJ: distance-based neighboring unit selection
-function mrWavCor = S_clu_wavcor__(S_clu, P, viClu_update)
-% symmetric matrix and common basis comparison only
-
-fUsePeak2 = 0;
-nInterp_merge = get_set_(P, 'nInterp_merge', 1); % set to 1 to disable
-fDrift_merge = get_set_(P, 'fDrift_merge', 0);
-P.fGpu = 0;
-nShift = ceil(P.spkRefrac_ms / 1000 * P.sRateHz * nInterp_merge); % +/-n number of samples to compare time shift
-% nShift = 0;
-fWaveform_raw = get_set_(P, 'fWavRaw_merge', 1);
-
-fZeroStart_raw = get_set_(P, 'fZeroStart_raw', 0);
-fRankCorr_merge = get_set_(P, 'fRankCorr_merge', 0);
-fMode_cor = 1; %0: pearson, 1:no mean subt pearson
-
-if nargin<3, viClu_update = []; end
-if ~isfield(S_clu, 'mrWavCor'), viClu_update = []; end
-fprintf('Computing waveform correlation...'); t1 = tic;
-if fWaveform_raw
-    tmrWav_clu = trim_spkraw_(S_clu.tmrWav_raw_clu, P);
-else
-    tmrWav_clu = S_clu.tmrWav_spk_clu;
-end
-% tmrWav_clu = ifeq_(fWaveform_raw, S_clu.tmrWav_raw_clu, S_clu.tmrWav_spk_clu);
-% tmrWav_clu = gpuArray_(tmrWav_clu, P.fGpu);
-if fDrift_merge && fWaveform_raw % only works on raw
-    tmrWav_lo_clu = trim_spkraw_(S_clu.tmrWav_raw_lo_clu, P);
-    tmrWav_hi_clu = trim_spkraw_(S_clu.tmrWav_raw_hi_clu, P);
-    ctmrWav_clu = {tmrWav_clu, tmrWav_lo_clu, tmrWav_hi_clu};
-else
-    ctmrWav_clu = {tmrWav_clu};
-end
-if fZeroStart_raw
-    ctmrWav_clu = cellfun(@(x)zero_start_(x), ctmrWav_clu, 'UniformOutput', 0);
-end
-if fRankCorr_merge
-    ctmrWav_clu = cellfun(@(x)rankorder_mr_(x,0), ctmrWav_clu, 'UniformOutput', 0);
-end
-if nInterp_merge>1
-    ctmrWav_clu = cellfun(@(x)interpft_(x, nInterp_merge), ctmrWav_clu, 'UniformOutput', 0);
-end
-nClu = S_clu.nClu;
-if fUsePeak2
-    [viSite_clu, viSite2_clu, viSite3_clu] = S_clu_peak2_(S_clu);
-    cviSite_clu = {viSite_clu, viSite2_clu, viSite3_clu};
-else
-    viSite_clu = S_clu.viSite_clu;
-    cviSite_clu = {viSite_clu};
-end
-mrWavCor = gpuArray_(zeros(nClu), P.fGpu);
-nSites_spk = P.maxSite*2+1-P.nSites_ref;
-nT = size(tmrWav_clu, 1);
-[cviShift1, cviShift2] = shift_range_(gpuArray_(int32(nT), P.fGpu), nShift);
-% viLags = 1:numel(cviShift1);
-if isempty(viClu_update)
-    vlClu_update = true(nClu, 1);
-    mrWavCor0 = [];
-    fParfor = 1;
-else
-    fParfor = 0;
-    vlClu_update = false(nClu, 1);
-    vlClu_update(viClu_update) = 1;
-    mrWavCor0 = S_clu.mrWavCor;
-    nClu_pre = size(mrWavCor0, 1);
-    vlClu_update((1:nClu) > nClu_pre) = 1;
-end
-cell_5args = {vlClu_update, cviShift1, cviShift2, mrWavCor0, fMode_cor};
-fParfor = 0; %debug
-if fParfor
-    try
-        parfor iClu2 = 1:nClu  %parfor speedup: 4x
-            vrWavCor2 = clu_wavcor__(ctmrWav_clu, cviSite_clu, P, cell_5args, iClu2);    
-            if ~isempty(vrWavCor2), mrWavCor(:, iClu2) = vrWavCor2; end
-        end
-    catch
-        fprintf('S_clu_wavcor_: parfor failed. retrying for loop\n');
-        fParfor = 0;
-    end
-end
-if ~fParfor    
-    for iClu2 = 1:nClu  %parfor speedup: 4x
-        vrWavCor2 = clu_wavcor__(ctmrWav_clu, cviSite_clu, P, cell_5args, iClu2);    
-        if ~isempty(vrWavCor2), mrWavCor(:, iClu2) = vrWavCor2; end
-    end
-end
-
-mrWavCor = max(mrWavCor, mrWavCor'); %make it symmetric
-mrWavCor(mrWavCor==0) = nan;
-mrWavCor = gather_(mrWavCor);
-fprintf('\ttook %0.1fs\n', toc(t1));
-end %func
-
-
-%--------------------------------------------------------------------------
-% 6/29/17 JJJ: distance-based neighboring unit selection
 function mrWavCor = S_clu_wavcor_(S_clu, P, viClu_update)
 % symmetric matrix and common basis comparison only
 
@@ -14112,6 +14022,7 @@ else
     fParfor = sum(vlClu_update) > 8;
 end
 
+fParfor = fParfor && get_set_(P, 'fParfor', 1);
 % fParfor = 0; %debug
 if fParfor
     try
@@ -14137,77 +14048,6 @@ mrWavCor(mrWavCor==0) = nan;
 mrWavCor = gather_(mrWavCor);
 fprintf('\ttook %0.1fs\n', toc(t1));
 end %func
-
-
-
-% --------------------------------------------------------------------------
-% 1/9/18 JJJ: more drift resistent version using mrPos_clu (median centroid  position)
-% function mrWavCor = S_clu_wavcor_1_(S_clu, P, viClu_update)
-% % symmetric matrix and common basis comparison only
-% global tnWav_raw
-% 
-% nInterp_merge = get_set_(P, 'nInterp_merge', 1); % set to 1 to disable
-% P.fGpu = 0;
-% nShift = ceil(P.spkRefrac_ms / 1000 * P.sRateHz * nInterp_merge); % +/-n number of samples to compare time shift
-% fWaveform_raw = get_set_(P, 'fWavRaw_merge', 1);
-% 
-% fMode_cor = 1; %0: pearson, 1:no mean subt pearson
-% 
-% if nargin<3, viClu_update = []; end
-% if ~isfield(S_clu, 'mrWavCor'), viClu_update = []; end
-% fprintf('Computing waveform correlation...\n\t'); t1 = tic;
-% if fWaveform_raw
-%     tmrWav_clu = trim_spkraw_(S_clu.tmrWav_raw_clu, P);
-% else
-%     tmrWav_clu = S_clu.tmrWav_spk_clu;
-% end
-% nShift = ceil(P.spkRefrac_ms / 1000 * P.sRateHz); % +/-n number of samples to compare time shift
-% 
-% nClu = S_clu.nClu;
-% mrWavCor = gpuArray_(zeros(nClu), P.fGpu);
-% if isempty(viClu_update)
-%     vlClu_update = true(nClu, 1);
-%     mrWavCor0 = [];
-%     fParfor = 1;
-% else
-%     fParfor = 0;
-%     vlClu_update = false(nClu, 1);
-%     vlClu_update(viClu_update) = 1;
-%     mrWavCor0 = S_clu.mrWavCor;
-%     nClu_pre = size(mrWavCor0, 1);
-%     vlClu_update((1:nClu) > nClu_pre) = 1;
-% end
-% 
-% maxDist_site_um = get_set_(P, 'maxDist_site_um', 50);
-% S0 = get0_();
-% mlNear_clu = squareform(pdist(S_clu.mrPos_clu')) <= maxDist_site_um; % don't use center sites but use centroid positions
-% S_clu_wav_pair_(); %init
-% S_clu.cviSpk_clu = arrayfun(@(iClu)int32(find(S_clu.viClu == iClu)), 1:nClu, 'UniformOutput', 0); % make it more efficient
-% for iClu2 = 1:nClu  %todo: use parfor
-%     % find other clusters that overlaps
-%     viClu1 = find(mlNear_clu(:,iClu2));
-%     viClu1(viClu1 >= iClu2) = []; % symmetric matrix comparison
-%     if isempty(viClu1), continue; end   
-%     if isempty(S_clu.cviSpk_clu{iClu2}), continue; end
-%     for iClu11 = 1:numel(viClu1)
-%         iClu1 = viClu1(iClu11);
-%         if ~vlClu_update(iClu1) && ~vlClu_update(iClu2)
-%             mrWavCor(iClu1, iClu2) = mrWavCor0(iClu1, iClu2);
-%         else            
-%             [mrWav1_, mrWav2_] = S_clu_wav_pair_(S_clu, S0, tnWav_raw, iClu1, iClu2);
-%             if isempty(mrWav1_) || isempty(mrWav2_), continue; end
-%             mrWavCor(iClu1, iClu2) = corr_(mrWav1_(:), mrWav2_(:));
-% %             mrWavCor(iClu1, iClu2) = corr_lag_(mrWav1_, mrWav2_, nShift);
-%         end        
-%     end
-%     fprintf('.');
-% end %iClu2 loop
-% 
-% mrWavCor = max(mrWavCor, mrWavCor'); %make it symmetric
-% mrWavCor(mrWavCor==0) = nan;
-% mrWavCor = gather_(mrWavCor);
-% fprintf('\n\ttook %0.1fs\n', toc(t1));
-% end %func
 
 
 %--------------------------------------------------------------------------
@@ -18973,7 +18813,7 @@ end %func
 % 9/29/17 JJJ: Displaying the version number of the program and what's used. #Tested
 function [vcVer, vcDate, vcVer_used] = version_(vcFile_prm)
 if nargin<1, vcFile_prm = ''; end
-vcVer = 'v4.0.2';
+vcVer = 'v4.0.3';
 vcDate = '8/3/2018';
 vcVer_used = '';
 if nargout==0
@@ -19878,70 +19718,6 @@ end %func
 
 
 %--------------------------------------------------------------------------
-% 2017/12/5 JJJ: distance-based neighboring unit selection
-% function mrWavCor = S_clu_wavcor_(S_clu, P, viClu_update)
-% % version selector
-% fUse_old = 1;
-% 
-% spkLim_raw_factor = get_set_(P, 'spkLim_raw_factor', 2);
-% spkLim_factor_merge = get_set_(P, 'spkLim_factor_merge', 1);
-% if nargin<3, viClu_update = []; end
-% 
-% if spkLim_factor_merge < spkLim_raw_factor && ~fUse_old
-%     mrWavCor = S_clu_wavcor_2_(S_clu, P, viClu_update); % newer format v3.1.8
-% else
-%     mrWavCor = S_clu_wavcor_1__(S_clu, P, viClu_update); % older format
-% end
-% end %func
-
-
-%--------------------------------------------------------------------------
-% 2017/12/5 JJJ: distance-based neighboring unit selection
-function mrWavCor = S_clu_wavcor_2_(S_clu, P, viClu_update) % works for only the latest format
-% use extra padding for spkRaw to time-shift nShift
-
-P.fGpu = 0;
-
-if nargin<3, viClu_update = []; end
-if ~isfield(S_clu, 'mrWavCor'), viClu_update = []; end
-fprintf('Computing waveform correlation...'); t1 = tic;
-ctmrWav_clu = {S_clu.tmrWav_raw_clu, S_clu.tmrWav_raw_lo_clu, S_clu.tmrWav_raw_hi_clu};
-
-nClu = S_clu.nClu;
-mrWavCor = gpuArray_(zeros(nClu), P.fGpu);
-if isempty(viClu_update)
-    vlClu_update = true(nClu, 1);
-    mrWavCor0 = [];
-else
-    vlClu_update = false(nClu, 1);
-    vlClu_update(viClu_update) = 1;
-    mrWavCor0 = S_clu.mrWavCor;
-    nClu_pre = size(mrWavCor0, 1);
-    vlClu_update((1:nClu) > nClu_pre) = 1;
-end
-[cviShift1, cviShift2] = calc_shift_range_(P);
-cell_5args = {P, vlClu_update, mrWavCor0, cviShift1, cviShift2};
-try
-    parfor iClu2 = 1:nClu  %parfor speedup: 4x
-        vrWavCor2 = clu_wavcor_2_(ctmrWav_clu, S_clu.viSite_clu, iClu2, cell_5args);
-        if ~isempty(vrWavCor2), mrWavCor(:, iClu2) = vrWavCor2; end
-    end
-catch
-    fprintf('S_clu_wavcor_: parfor failed. retrying for loop\n');
-    for iClu2 = 1:nClu  %parfor speedup: 4x
-        vrWavCor2 = clu_wavcor_2_(ctmrWav_clu, S_clu.viSite_clu, iClu2, cell_5args);
-        if ~isempty(vrWavCor2), mrWavCor(:, iClu2) = vrWavCor2; end
-    end
-end
-
-mrWavCor = max(mrWavCor, mrWavCor'); %make it symmetric
-mrWavCor(mrWavCor==0) = nan;
-mrWavCor = gather_(mrWavCor);
-fprintf('\ttook %0.1fs\n', toc(t1));
-end %func
-
-
-%--------------------------------------------------------------------------
 % 10/27/17 JJJ: distance-based neighboring unit selection
 function vrWavCor2 = clu_wavcor_2_(ctmrWav_clu, viSite_clu, iClu2, cell_5args)
 [P, vlClu_update, mrWavCor0, cviShift1, cviShift2] = deal(cell_5args{:});
@@ -20802,6 +20578,10 @@ vcFile_prm = subsFileExt_(vcFile_mda, ['_', vcPostfix, '.prm']);
 vcFile_prm = subsDir_(vcFile_prm, format_dir_(vcDir_prm)); % write meta file based on .mda
 P.vcFile_prm = vcFile_prm;
 
+% Make it work with cluster environment (slurm srun)
+P.fSavePlot_RD = get_set_(P, 'fSavePlot_RD', 0); % disable plot output
+P.fParfor = get_set_(P, 'fParfor', 0); % disable parallel 
+
 % Overload with template and default
 P0 = file2struct_(ircpath_(read_cfg_('default_prm')));  %P = defaultParam();
 P0 = struct_merge_(P0, file2struct_(vcFile_template)); 
@@ -20809,8 +20589,6 @@ P = struct_merge_(P0, P);
 P.duration_file = S_mda.dimm(2) / P.sRateHz; %assuming int16
 P.version = version_();
 
-% disable plot output
-P.fSavePlot_RD = 0; % Setting this to 1 will crash cluster environment
 
 % Write to prm file
 try
