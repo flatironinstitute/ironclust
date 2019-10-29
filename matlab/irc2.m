6beab6be8f5e13c7c3f142e72339079138250b4d
James Jun <jamesjun@gmail.com>
2019-10-28 21:11:53 -0700

    version number incremented

--- matlab/irc2.m
+++ matlab/irc2.m
@@ -265,8 +265,8 @@ end %func
 %--------------------------------------------------------------------------
 % 11/6/18 JJJ: Displaying the version number of the program and what's used. #Tested
 function [vcVer, vcDate, vcHash] = version_()
-vcVer = 'v5.0.6';
-vcDate = '10/25/2019';
+vcVer = 'v5.0.7';
+vcDate = '10/28/2019';
 vcHash = file2hash_();
 
 if nargout==0
