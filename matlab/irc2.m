f07b8d48ef79f7d27a273f09c981cca0fc512950
James Jun <jamesjun@gmail.com>
2019-11-19 12:18:35 -0800

    speed improvement on feature extraction

--- matlab/irc2.m
+++ matlab/irc2.m
@@ -2578,18 +2578,17 @@ function trPc_spk = project_pc_(trWav_spk, mrPv, P)
 project_ = @(x,y)reshape(x' * reshape(y, size(y,1), []), size(x,2), size(y,2), size(y,3));
 trPc_spk = project_(mrPv, trWav_spk); % project 0-shift
 
-if get_set_(P, 'fInterp_fet', 0) == 0, return; end
-    
 % interpolate and shift
-nPc = size(mrPv,2);
-switch 1
-    case 3
-        viShift = [0, -1,-.5,.5,1,1.5,-1.5];     
-    case 2
-        viShift = [0, -1,-.5,.5,1,1.5,-1.5,2,-2]; 
-    case 1
-        viShift = [0, -1,-.5,.5,1]; 
+if get_set_(P, 'fInterp_fet', 0) == 0, return; end   
+switch 1 % 1
+    case 0, return;
+    case 1, viShift = -1:.5:1;
+    case 2, viShift = -1.5:.5:1.5;     
+    case 3, viShift = -2:.5:2;       
+    case 4, viShift = -1:.25:1;
 end
+viShift = [0, setdiff(viShift,0)]; % put 0 first
+nPc = size(mrPv,2);
 trPv_shift = arrayfun(@(x)vr2mr_shift_(mrPv(:,x), viShift), 1:nPc, 'UniformOutput', 0);
 trPv_shift = permute(cat(3, trPv_shift{:}),[1,3,2]);
 
@@ -2598,12 +2597,10 @@ mrWav1 = squeeze_(trWav_spk(:,1,:), 2); % first channel
 mrPv1_shift = squeeze_(trPv_shift(:,1,:),2); %first pv shift
 [~, viMax_spk] = max(abs(mrPv1_shift' * mrWav1));  % project chan1 shift   
 
-for iPc = 1:nPc
-    for iShift=2:numel(viShift)
-        viSpk1 = find(viMax_spk == iShift);
-        if isempty(viSpk1), continue; end
-        trPc_spk(:,:,viSpk1) = project_(trPv_shift(:,:,iShift), trWav_spk(:,:,viSpk1));
-    end %for
+for iShift=2:numel(viShift) % start from 2 to exclude shifting 0
+    viSpk1 = find(viMax_spk == iShift);
+    if isempty(viSpk1), continue; end
+    trPc_spk(:,:,viSpk1) = project_(trPv_shift(:,:,iShift), trWav_spk(:,:,viSpk1));
 end %for
 end %func
 
@@ -2639,9 +2636,10 @@ switch 2
         vrD1 = flipud(diag(vrD1));
     case 2
         [mrPv1, ~, vrD1] = pca(gather_(mr1)','Centered',false, 'NumComponents', nPc_spk);
-%         fh_proj = @(x)(mrPv' * x)';
-%         mr2 = mrPv * mrPc'; % confirm the agreement
-%         mr3 = mrPv * (mrPv' * mr1);
+%     case 3
+%         [mrX1, mrX2] = deal(squeeze(tr(:,1,viSpk_sub)), squeeze(tr(:,end,viSpk_sub)));
+%         [mrA, mrB, r] = canoncorr_(mrX1', mrX2', 0);
+%         fb = wavelets(dwtfilterbank('Wavelet','sym4','SignalLength',size(tr,1),'Level',5,'FilterType','analysis'));
 end
 % spike center should be negative
 % iMid = 1-P.spkLim(1);
@@ -2650,6 +2648,60 @@ end
 % trFft = fft(tr);
 % trFft = abs(trFft(2:end/2,:,:));
 % figure; imagesc(std(trFft,1,3))
+end %func
+
+
+%--------------------------------------------------------------------------
+function [A,B,r] = canoncorr_(X,Y, fCentered)
+if nargin < 2
+    error(message('stats:canoncorr:TooFewInputs'));
+end
+if nargin<3, fCentered = 1; end
+
+[n,p1] = size(X);
+if size(Y,1) ~= n
+    error(message('stats:canoncorr:InputSizeMismatch'));
+elseif n == 1
+    error(message('stats:canoncorr:NotEnoughData'));
+end
+p2 = size(Y,2);
+
+% Center the variables
+if fCentered
+    X = X - repmat(mean(X,1), n, 1);
+    Y = Y - repmat(mean(Y,1), n, 1);
+end
+% Factor the inputs, and find a full rank set of columns if necessary
+[Q1,T11,perm1] = qr(X,0);
+rankX = sum(abs(diag(T11)) > eps(abs(T11(1)))*max(n,p1));
+if rankX == 0
+    error(message('stats:canoncorr:BadData', 'X'));
+elseif rankX < p1
+    warning(message('stats:canoncorr:NotFullRank', 'X'));
+    Q1 = Q1(:,1:rankX); T11 = T11(1:rankX,1:rankX);
+end
+[Q2,T22,perm2] = qr(Y,0);
+rankY = sum(abs(diag(T22)) > eps(abs(T22(1)))*max(n,p2));
+if rankY == 0
+    error(message('stats:canoncorr:BadData', 'Y'));
+elseif rankY < p2
+    warning(message('stats:canoncorr:NotFullRank', 'Y'));
+    Q2 = Q2(:,1:rankY); T22 = T22(1:rankY,1:rankY);
+end
+
+% Compute canonical coefficients and canonical correlations.  For rankX >
+% rankY, the economy-size version ignores the extra columns in L and rows
+% in D. For rankX < rankY, need to ignore extra columns in M and D
+% explicitly. Normalize A and B to give U and V unit variance.
+d = min(rankX,rankY);
+[L,D,M] = svd(Q1' * Q2,0);
+A = T11 \ L(:,1:d) * sqrt(n-1);
+B = T22 \ M(:,1:d) * sqrt(n-1);
+r = min(max(diag(D(:,1:d))', 0), 1); % remove roundoff errs
+
+% Put coefficients back to their full size and their correct order
+A(perm1,:) = [A; zeros(p1-rankX,d)];
+B(perm2,:) = [B; zeros(p2-rankY,d)];
 end %func
 
 
