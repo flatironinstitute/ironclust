014c640fc9469dd6d5b30ae6a7649d39a878024d
James Jun <jamesjun@gmail.com>
2020-01-24 20:54:31 -0500

    compare script added

---
+++ matlab/scripts/S200110_compare_firings_mda.m
@@ -0,0 +1,16 @@
+% sRateHz = 30000;
+% load_firings_ = @(x)irc2('call', 'load_firings_', {x, sRateHz});
+% S_irc = load_firings_('D:\Globus\recordings\kf19_nt27\irc2\firings.mda');
+
+
+% S_ms4 = load_firings_('D:\Globus\recordings\kf19_nt27\kf19_nt27.firings_orig.mda');
+% describe firings.mda
+
+%% validate S_irc using S_ms4
+
+% preselect from S_irc, remove durations that does not overlap
+% skip clusters if there is no overlap what so ever
+
+vcFile_gt_mda = 'D:\Globus\recordings\kf19_nt27\kf19_nt27.firings_orig.mda';
+vcFile_clu_mda = 'D:\Globus\recordings\kf19_nt27\irc2\firings.mda';
+irc2('compare-mda', vcFile_gt_mda, vcFile_clu_mda); % 3 hours
