5aa3cd27e8636e354bcae2f8562abb30e656d416
James Jun <jamesjun@gmail.com>
2020-01-28 22:55:47 -0500

    cc_merge_thresh=.9 raised based on spikeforest test

--- matlab/scripts/S200110_compare_firings_mda.m
+++ matlab/scripts/S200110_compare_firings_mda.m
@@ -11,6 +11,8 @@
 % preselect from S_irc, remove durations that does not overlap
 % skip clusters if there is no overlap what so ever
 
+irc2 auto D:\Globus\recordings\kf19_nt27\irc2\raw_geom.prm
+
 vcFile_gt_mda = 'D:\Globus\recordings\kf19_nt27\kf19_nt27.firings_orig.mda';
 vcFile_clu_mda = 'D:\Globus\recordings\kf19_nt27\irc2\firings.mda';
 irc2('compare-mda', vcFile_gt_mda, vcFile_clu_mda); % 3 hours
