cebfbdccc6df2bb355d313d1df0d1855db89fb70
James Jun <jamesjun@gmail.com>
2020-02-20 15:28:23 -0500

    irc2 optimize

--- matlab/irc2.m
+++ matlab/irc2.m
@@ -8372,7 +8372,7 @@ end %func
 %--------------------------------------------------------------------------
 function optimize_status_(vcDir_rec, vcFile_prmset)
 try
-    if exist_file_(vcDir_rec), vcDir_rec=fullfile(vcDir_rec); end 
+    if exist_file_(vcDir_rec), vcDir_rec=fileparts(vcDir_rec); end 
     
     assert(exist_file_(vcFile_prmset) && exist_dir_(vcDir_rec), 'file or dir does not exist');
 
@@ -8423,7 +8423,7 @@ function optimize_clear_(vcDir_rec, vcFile_prmset)
 % optimize_clear_(vcFile_list, vcFile_prmset)
 % optimize_clear_(vcDir_rec, vcFile_prmset)
 
-if exist_file_(vcDir_rec), vcDir_rec=fullfile(vcDir_rec); end 
+if exist_file_(vcDir_rec), vcDir_rec=fileparts(vcDir_rec); end 
 assert(exist_file_(vcFile_prmset) && exist_dir_(vcDir_rec), 'file or dir does not exist');
 
 vcSorter = lower(strrep(vcFile_prmset, '.prmset', ''));
